fec5bc94d18a06a40e5513bcec1b0bcda61b1780
Koch13o1 <maitreyakoch123@gmail.com>
2025-05-19 17:37:42 -0400

    Handled context after login, Added dashboard after login and graph functionality

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -4,6 +4,10 @@ import LandingPage from './components/LandingPage'
 import AboutPage from './components/AboutPage'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import RegisterPage from './components/RegisterPage'
+import { Toaster } from 'react-hot-toast'
+import DashboardLayout from './Dashboard/DashboardLayout'
+import LoginPage from './components/LoginPage'
 
 function App() {
 
@@ -11,9 +15,13 @@ function App() {
     <>
       <Router>
         <NavBar />
+        <Toaster position='bottom-center'/>
         <Routes>
           <Route path='/' element={<LandingPage />} />
           <Route path='/about' element={<AboutPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/dashboard' element={<DashboardLayout />} />
         </Routes>
         <Footer />
       </Router>
